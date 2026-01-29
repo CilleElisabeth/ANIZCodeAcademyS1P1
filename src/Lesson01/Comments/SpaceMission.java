ad075d6f458320e0be8c1a7a421796ec5a35e790
AnikoZs <azsigri@gmail.com>
2026-01-29 09:52:00 +0100

    Original

---
+++ src/Lesson01/Comments/SpaceMission.java
@@ -0,0 +1,36 @@
+package Lesson01.Comments;
+
+public class SpaceMission {
+
+    public static void main(String[] args) {
+
+        SpaceMission mission = new SpaceMission();
+
+
+        mission.printTitle();
+        mission.printAstronautInfo();
+    }
+
+
+    public void printTitle() {
+        System.out.println("================================");
+        System.out.println("  Andreas Mogensen's Space Trip ");
+        System.out.println("================================\n");
+    }
+
+
+    public void printAstronautInfo() {
+
+        String name = "Andreas Mogensen";
+        String nationality = "Danish";
+        String mission = "International Space Station (ISS)";
+
+
+        System.out.println("Astronaut name: " + name);
+        System.out.println("Nationality: " + nationality);
+        System.out.println("Mission: \"" + mission + "\"");
+        System.out.println("\nMessage from space:");
+        System.out.println("\"Hello Earth! I am coding from space!\"");
+    }
+}
+
---
+++ src/Lesson01/Comments/SpaceMissionSolution.java
@@ -0,0 +1,68 @@
+package Lesson01.Comments;
+
+public class SpaceMissionSolution {
+
+        // The main method is the starting point of every Java program
+        public static void main(String[] args) {
+
+            // Create an object of the SpaceMission class
+            // This allows us to use the methods in this class
+            SpaceMissionSolution mission = new SpaceMissionSolution();
+
+            // Call a method that prints the title
+            mission.printTitle();
+
+            // Call a method that prints information about the astronaut
+            mission.printAstronautInfo();
+        }
+
+        // This method prints a title for the program
+        // It does not return anything, so the return type is void
+        public void printTitle() {
+
+            // Print a line of "=" characters
+            System.out.println("================================");
+
+            // Print the title text
+            System.out.println("  Andreas Mogensen's Space Trip ");
+
+            // Print another line of "=" characters
+            System.out.println("================================");
+
+            // Print an empty line using an escape character (\n)
+            System.out.println("\n");
+        }
+
+        // This method prints information about the astronaut and the mission
+        public void printAstronautInfo() {
+
+            // Declare and initialize variables
+            // Variables with astronaut information
+            // These variables store information as text (String)
+            String name = "Andreas Mogensen";
+            String nationality = "Danish";
+            String mission = "International Space Station (ISS)";
+
+            // Print the astronaut's name
+            System.out.println("Astronaut name: " + name);
+
+            // Print the nationality
+            System.out.println("Nationality: " + nationality);
+
+            // Print the mission
+            // The quotation marks are printed using escape characters (\")
+            System.out.println("Mission: \"" + mission + "\"");
+
+            // Print an empty line to make the output easier to read
+            System.out.println("\n");
+
+            // Print a message header
+            System.out.println("Message from space:");
+
+            // Print a message using quotation marks and text
+            System.out.println("\"Hello Earth! I am coding from space!\"");
+        }
+    }
+
+
+
